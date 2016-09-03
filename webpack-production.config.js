18e3a5b796856f3e6cf81958641767679f19b970
Pouja Nikray <poujanik@gmail.com>
2016-09-03 17:16:18 +0200

    It now hashes the file when deploying
    Closes #9

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -2,8 +2,9 @@ const webpack = require('webpack');
 const path = require('path');
 const buildPath = path.resolve(__dirname, 'build');
 const srcPath = path.resolve(__dirname, 'src');
-const TransferWebpackPlugin = require('transfer-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var Path = require("path");
+var FileSystem = require("fs");
 
 const config = {
     entry: [path.join(__dirname, '/src/Index.js')],
@@ -12,10 +13,10 @@ const config = {
     // output config
     output: {
         path: buildPath, // Path of output file
-        filename: 'app.js', // Name of output file
+        filename: 'app.[hash].js', // Name of output file
     },
     plugins: [
-        new ExtractTextPlugin('main.css'),
+        new ExtractTextPlugin('main.[hash].css'),
         // Define production build to allow React to strip out unnecessary checks
         new webpack.DefinePlugin({
             'process.env': {
@@ -29,22 +30,35 @@ const config = {
                 warnings: false,
             },
         }),
-        // Allows error warnings but does not stop compiling.
-        // Transfer Files
-        new TransferWebpackPlugin([{
-            from: 'www'
-        }, ], path.resolve(__dirname, '.')),
+        function() {
+            // replaces app.js and main.css with app.[hash].js and main.[hash].css respectively
+            this.plugin("done", function(statsData) {
+                var stats = statsData.toJson();
+                if (!stats.errors.length) {
+                    var htmlFileName = "index.html";
+                    var html = FileSystem.readFileSync(Path.join(__dirname, "www" ,htmlFileName), "utf8");
+
+                    // Hacky solution since this will work only with the current configuration
+                    // If there are more bundles it will fail to do this
+                    var htmlOutput = html.replace("app.js", stats.assetsByChunkName.main[0])
+                        .replace("main.css", stats.assetsByChunkName.main[1]);
+                    FileSystem.writeFileSync(
+                        Path.join(__dirname, "build", htmlFileName),
+                        htmlOutput);
+                }
+            });
+        }
     ],
     module: {
         loaders: [{
             test: /\.js$/, // All .js files
+            include: [srcPath],
             loader: 'webpack-replace',
             query: {
                 search: 'http://localhost:8000',
                 replace: ''
             }
-        },
-        {
+        }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
             include: [srcPath]
